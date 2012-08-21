007825910c866c96599d9c1c0fe78c60f55646ee
Martin Darmüntzel <git@trivialanalog.de>
2012-08-21 18:47:19 +0200

    Kleine Änderungen.
    - Menüpunkte entfernt
    - Datum eines Posts wird angezeigt
    - deutsche Datumsanzeige
    - Posts werden absteigend nach Datum ausgegeben

--- trivialblog/views.py
+++ trivialblog/views.py
@@ -1,13 +1,16 @@
+import locale
+locale.setlocale(locale.LC_ALL, 'de_DE')
+
 from pyramid.view import view_config
 from markdown import markdown
+from sqlalchemy import desc
 
 from .models import (
     DBSession,
     Post,
     )
 
-#@view_config(route_name='home', renderer='templates/basic.jinja2')
 @view_config(route_name='home', renderer='templates/basic.jinja2')
 def home(request):
-    posts = DBSession.query(Post).all()
+    posts = DBSession.query(Post).order_by(Post.pdate.desc()).all()
     return dict(posts=posts)
