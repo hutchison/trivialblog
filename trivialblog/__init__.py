736f3637dea1df146ed2f1bcbcd399acd54c3afa
Martin Darmüntzel <git@trivialanalog.de>
2012-08-22 10:52:04 +0200

    Fügt "bestimmten Post anschauen" Funktion hinzu.

--- trivialblog/__init__.py
+++ trivialblog/__init__.py
@@ -12,5 +12,6 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('view_post', '/p/{id}')
     config.scan()
     return config.make_wsgi_app()
--- trivialblog/views.py
+++ trivialblog/views.py
@@ -5,6 +5,11 @@ from pyramid.view import view_config
 from markdown import markdown
 from sqlalchemy import desc
 
+from pyramid.httpexceptions import (
+    HTTPFound,
+    HTTPNotFound,
+    )
+
 from .models import (
     DBSession,
     Post,
@@ -12,5 +17,18 @@ from .models import (
 
 @view_config(route_name='home', renderer='home.jinja2')
 def home(request):
+    """Die Startseite.
+        Zeigt die letzten 10 Posts an."""
     posts = DBSession.query(Post).order_by(Post.pdate.desc()).limit(10)
     return dict(posts=posts)
+
+@view_config(route_name='view_post', renderer='view_post.jinja2')
+def view_post(request):
+    """Zeigt einen bestimmten Post an."""
+    postid = request.matchdict['id']
+    post = DBSession.query(Post).filter_by(id=postid).first()
+    if post is None:
+        return HTTPNotFound('Kein Blogpost mit der ID ' + str(postid) +
+            ' vorhanden.')
+    else:
+        return dict(p=post)
